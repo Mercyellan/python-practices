dc5821a830090ac17804e736f994f0d9c04a285a
Mercyellan <mercyodoh25@gmail.com>
2026-04-01 16:41:57 +0100

    loops demo

---
+++ Loops.py
@@ -0,0 +1,14 @@
+# # Example: Iterating through a list of fruits
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
+
+# # Example: Iterating through a list of fruits
+# fruits = ["apple", "banana", "cherry"]
+# for fruit in fruits:
+#     print(fruit)
+
+    # Example: Iterating through a string
+message = "Hello"
+for char in message:
+    print(char)
