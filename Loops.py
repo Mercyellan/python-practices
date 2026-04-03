1c875077d95e4ae829d69e5de7dbe2b0a2ece539
Mercyellan <mercyodoh25@gmail.com>
2026-04-03 14:47:58 +0100

    learning about range

--- Loops.py
+++ Loops.py
@@ -3,12 +3,22 @@
 # for fruit in fruits:
 #     print(fruit)
 
-# # Example: Iterating through a list of fruits
-# fruits = ["apple", "banana", "cherry"]
-# for fruit in fruits:
-#     print(fruit)
+#     # Example: Iterating through a string
+# message = "Hello"
+# for char in message:
+#     print(char)
 
-    # Example: Iterating through a string
-message = "Hello"
-for char in message:
-    print(char)
+
+# Example: Using range() to print numbers from 0 to 4
+for i in range(5):
+    print(i)
+# Example: Using range() to print numbers from 2 to 5
+for i in range(2, 6):
+    print(i)
+# Example: Using range() to print even numbers from 0 to 100
+for i in range(0, 11, 2):
+    print(i)
+
+    # Example: Using range() to print even numbers from 0 to 2000
+for i in range(0, 11, 2):
+    print(i)
