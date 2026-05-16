03bc9c259b9cff90ab203709e095e72186e91115
Mercyellan <mercyodoh25@gmail.com>
2026-05-16 03:52:29 +0100

    more variable examples done

--- variables.py
+++ variables.py
@@ -1,14 +1,45 @@
-x = y = z = 0
-print ('x, y, z')
+# # x = y = z = 0
+# # print ('x, y, z')
 
-a, b, c = 1, 2, "hello"
-print ('a, b, c')
 
-#constants 
-gravity =98.1
-mass =5
-height =10
+
+# a, b, c = 1, 2, "hello"
+# print ('a, b, c')
+
+
+
+# #constants 
+# gravity =98.1
+# mass =5
+# height =10
+# potential_energy = mass * gravity * height
+# print (potential_energy)
+
+
+
+# x = y = z = 0
+# print (x)
+
+
+# a, b, c = 1, 2, "hello"
+# print (a, b, c)
+
+
+python = "language"
+print (python)
+
+
+
+value= 100
+print (value)
+
+a = 10
+b = ("hello")
+c= 3.14
+print (a, b)
+
+gravity = 9.81
+height = 15
+mass =10
 potential_energy = mass * gravity * height
 print (potential_energy)
-
-#
